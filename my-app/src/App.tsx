e004bd96abbed66c1dcc8ed917c98d23cf052d75
Romans <romanich1998@gmail.com>
2022-11-29 22:26:54 +0100

    fix

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,16 +1,6 @@
-import { ThemeProvider } from "@mui/material";
 import "./App.css";
-import Footer from "./components/Footer";
-import { theme } from "./theme/theme";
-import { ChangeEvent, useCallback, useEffect, useState } from "react";
-import { Movie } from "./models/Movie";
-import { searchMovies } from "./middleware/searchMovies";
-import { useDebounce } from "./middleware/useDebounce";
-import AppRouter from "./components/AppRouter";
 import Header from "./components/Header";
-// import {getTopRated} from "./middleware/getTopRated";
-// import {getCredits, getPerson, getPersonByID, searchPerson} from "./middleware/movieCredits";
-import {Button, TextField, ThemeProvider} from "@mui/material";
+import {TextField, ThemeProvider} from "@mui/material";
 import "./App.css";
 import Footer from "./components/Footer";
 import { theme } from "./theme/theme";
@@ -19,24 +9,17 @@ import AppRouter from "./components/AppRouter";
 
 function App() {
   const [radioButtons, setRadioButtons] = useState(["movie"]);
-  const [message, setMessage] = useState<string>("");
-
-  const handleRadioButtons = () => {
-    if (radioButtons[0] == "movie") {
-      setRadioButtons(["person"]);
-    } else {
-      setRadioButtons(["movie"]);
-    }
-  };
+  // const handleRadioButtons = () => {
+  //   if (radioButtons[0] == "movie") {
+  //     setRadioButtons(["person"]);
+  //   } else {
+  //     setRadioButtons(["movie"]);
+  //   }
+  // };
 
   localStorage.setItem("userID", "23");
 
   console.log(localStorage.getItem("userID"));
-
-  const handleSearch = async (message: string) => {
-    const temp = await searchMovies(message);
-    setMovies(temp.results);
-  };
 
   const handleMessageChange = async (e: ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value)
@@ -47,14 +30,15 @@ function App() {
 
   return (
     <>
+
       <ThemeProvider theme={theme}>
         <Header />
+        <AppRouter />
+        <Footer />
         <TextField
             name={"search"}
             onChange={handleMessageChange}
-          />
-        <AppRouter />
-        <Footer />
+        />
       </ThemeProvider>
     </>
   );
