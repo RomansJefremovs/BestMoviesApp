8f17c0ad9e2ed9f2042668e814031fdd120517f7
Romans <romanich1998@gmail.com>
2022-11-30 23:39:54 +0100

    signin fix

---
+++ my-app/src/middleware/login.ts
@@ -0,0 +1,16 @@
+
+export const login = async (username:string, password:string) => {
+    const url = `https://cloudcomputingapi.azurewebsites.net/User/login/${username}`
+    try {
+        const temp = await fetch(url,{
+            method:"POST",
+            body:`"${password}"`,
+            headers:{
+                "Content-Type": 'application/json'
+            }
+        })
+       return temp.json()
+    }catch (e){
+        return e
+    }
+}
---
+++ my-app/src/middleware/register.ts
@@ -0,0 +1,15 @@
+export const register = async (username:string, password:string) => {
+    const url = `https://cloudcomputingapi.azurewebsites.net/User/create/${username}`
+    try {
+        const temp = await fetch(url,{
+            method:"POST",
+            body:`"${password}"`,
+            headers:{
+                "Content-Type": 'application/json'
+            }
+        })
+        return temp.text()
+    }catch (e){
+        return e
+    }
+}
