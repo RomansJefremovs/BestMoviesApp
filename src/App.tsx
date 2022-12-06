4e56d55b53167606093e47a4d45a90ed26b638d5
Romans <romanich1998@gmail.com>
2022-12-06 09:20:20 +0100

    signin error handling

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,10 @@ import "./App.css";
 import { ThemeProvider } from "@mui/material";
 import Footer from "./components/Footer";
 import { theme } from "./theme/theme";
-import { useEffect, useState} from "react";
+import { useState} from "react";
 import AppRouter from "./components/AppRouter";
 import NavBar from "./components/NavBar";
-import {createPlaylist} from "./middleware/PlaylistsMiddleware/createPlaylist";
-import {addMovieToList} from "./middleware/PlaylistsMiddleware/addMovieToList";
-import {removeMovieFromList} from "./middleware/PlaylistsMiddleware/removeMovieFromList";
-import {getUserLists} from "./middleware/PlaylistsMiddleware/getUserLists";
-import {getFavouritesIds} from "./middleware/getFavouritesIds";
-import {getPlaylist} from "./middleware/PlaylistsMiddleware/getPlaylist";
-import {editPlaylist} from "./middleware/PlaylistsMiddleware/editPlaylist";
-import {getAllPublicPlaylists} from "./middleware/PlaylistsMiddleware/getAllPublicPlaylists";
-import {checkIfMovieIsFavourite} from "./middleware/checkIfMovieIsFavourite";
+
 
 function App() {
   const userID = localStorage.getItem("userID");
@@ -21,15 +13,6 @@ function App() {
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
-  const tryout = async()=>{
-    // const temp = await getAllPublicPlaylists(2)
-    const temp = await checkIfMovieIsFavourite(1013860)
-    console.log(temp)
-  }
-  // localStorage.clear()
-  useEffect(()=>{
-    tryout()
-  },[])
   return (
     <>
       <ThemeProvider theme={theme}>
